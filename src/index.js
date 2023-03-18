5a41faac17dfa27403a022412fe8eba444038780
Duttji1996 <prakashdutttripathi@gmail.com>
2023-03-18 17:40:04 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
-const env = require('dotenv').config()
-const process = require('node:process')
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
+const env = require("dotenv").config();
+const process = require("node:process");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route)
+app.use("/", route);
 
-async function bootstrap(){
-try{
-   mongoose.set('strictQuery', false)
-await mongoose.connect(process.env.MONGO_URI)
-console.log("MongoDb is connected")
- app.listen(process.env.PORT , ()=>{
-console.log(`server is running on PORT ${process.env.PORT}`)
-  })
-}catch(error){
-console.log("db error", error.message)
-return res.status(500).json({message:error.message})
+async function bootstrap() {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDb is connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on PORT ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("db error", error.message);
+    return res.status(500).json({ message: error.message });
   }
 }
 
-bootstrap().finally()
+bootstrap().finally();
