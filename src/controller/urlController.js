bdea851205e99422a53d4c6d9439ccd6ffab4b48
Duttji1996 <prakashdutttripathi@gmail.com>
2023-03-18 18:15:56 +0530

    ip modified

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -7,35 +7,46 @@ const CreateShortUrl = async function (req, res) {
     const body = req.body;
 
     if (Object.keys(body).length === 0) {
-      return res.status(400).send({ status: false, message: "Please enter the data" });
+      return res
+        .status(400)
+        .send({ status: false, message: "Please enter the data" });
     }
 
     if (!body.longUrl) {
-      return res.status(400).send({ status: false, message: "Please enter the URL" });
+      return res
+        .status(400)
+        .send({ status: false, message: "Please enter the URL" });
     }
 
     if (!validUrl.isWebUri(body.longUrl)) {
-      return res.status(400).send({ status: false, message: "Looks like not a valid URL" });
+      return res
+        .status(400)
+        .send({ status: false, message: "Looks like not a valid URL" });
     }
 
     const FindUrl = await urlModel.findOne({ longUrl: body.longUrl });
 
     if (FindUrl) {
-    return res.status(200).send({ status: true, message: FindUrl });
+      return res.status(200).send({ status: true, message: FindUrl });
     }
     const urlCode = shortid.generate().toLowerCase();
     const baseUrl = process.env.baseUrl;
-    const shortUrl = baseUrl + "/" + urlCode;
+    const shortUrl = "43.204.199.206:3000" + "/" + urlCode;
     // url = { longUrl, shortUrl, urlCode }
     body.shortUrl = shortUrl;
     body.urlCode = urlCode;
 
     await urlModel.create(body);
 
-    const ShowUrl = await urlModel.findOne({ longUrl: body.longUrl })
+    const ShowUrl = await urlModel.findOne({ longUrl: body.longUrl });
 
-    res.status(201).send({status: true,message: "URL create successfully",data: ShowUrl});
-
+    res
+      .status(201)
+      .send({
+        status: true,
+        message: "URL create successfully",
+        data: ShowUrl,
+      });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
@@ -43,12 +54,14 @@ const CreateShortUrl = async function (req, res) {
 
 const GetUrl = async function (req, res) {
   try {
-      const getUrl = await urlModel.findOne({ urlCode: req.params.urlCode });
-      if (!getUrl) {
-        return res.status(404).send({ status: false, message: "Url-Code not found" });
-      }
-      return res.status(307).redirect(getUrl.longUrl);
-  }catch (error) {
+    const getUrl = await urlModel.findOne({ urlCode: req.params.urlCode });
+    if (!getUrl) {
+      return res
+        .status(404)
+        .send({ status: false, message: "Url-Code not found" });
+    }
+    return res.status(307).redirect(getUrl.longUrl);
+  } catch (error) {
     res.status(500).send({ status: false, message: error.message });
   }
 };
--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ app.use("/", route);
 async function bootstrap() {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://prakash_tripathi:fl1z8GcN2hzLtWSO@cluster0.wgwx59o.mongodb.net/url-short");
     console.log("MongoDb is connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`server is running on PORT ${process.env.PORT}`);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is running on PORT ${process.env.PORT || 3000}`);
     });
   } catch (error) {
     console.log("db error", error.message);
